b26e4d1c7aa2131ec4ef4d765b81f254d51d8223
Victor Aprea <victor.aprea@wickeddevice.com>
2015-10-12 16:45:13 -0400

    updated version

--- AQEV2FW_PM.ino
+++ AQEV2FW_PM.ino
@@ -20,7 +20,7 @@
 // semantic versioning - see http://semver.org/
 #define AQEV2FW_MAJOR_VERSION 2
 #define AQEV2FW_MINOR_VERSION 0
-#define AQEV2FW_PATCH_VERSION 4
+#define AQEV2FW_PATCH_VERSION 5
 
 #define MQTT_TOPIC_PREFIX "/orgs/wd/aqe/"
 #define DEVICE_NAME "CC3000" // this is used for smart config
